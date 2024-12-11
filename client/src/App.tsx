315097e0b57bd5c360a0744b3b11ede42a5498f2
Govind Gupta <govind12771277@gmail.com>
2024-12-12 04:57:14 +0530

    frontend: signup login working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { lazy } from "react"
+
+
+const Home = lazy(() => import("./pages/Home"))
+const Login = lazy(() => import("./pages/Login"))
+const Chat = lazy(() => import("./pages/Chat"))
+const Groups = lazy(() => import("./pages/Groups"))
 
 
 const App = () => {
   return (
-    <div>
-      new web page
-    </div>
+    <main className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/groups" element={<Groups />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
   )
 }
 
---
+++ client/src/pages/Groups.tsx
@@ -0,0 +1,10 @@
+
+const Groups = () => {
+  return (
+    <div>
+      <h1>Group</h1>
+    </div>
+  )
+}
+
+export default Groups
