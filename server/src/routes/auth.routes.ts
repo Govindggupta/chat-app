dde13ed30db4eb251513572591b6973a41bb49a0
Govind Gupta <govind12771277@gmail.com>
2024-12-19 18:42:52 +0530

    backend: login , register added

--- server/src/routes/auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.get("/login" , (req, res) => {
-    res.status(200).json({message: "Login"});
-});
-
-export default router;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import authRoutes from "./routes/auth.routes.js"
-dotenv.config({ path: './.env' });
+import authRoutes from "./routes/auth.route.js";
+import connectMongoDB from './db/db.js';
+
+dotenv.config({
+    path: './.env'
+});
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/auth", authRoutes);
 
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    connectMongoDB();
+    console.log(`Server is running on port ${PORT}...`);
 });
