5312eea74716da00a4974e4a0d3c4eb61b430e49
Govind Gupta <govind12771277@gmail.com>
2024-12-20 14:53:47 +0530

    backend : message routes and controllers added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import connectMongoDB from './db/db.js';
 import cookieparser from 'cookie-parser';
 
@@ -14,7 +15,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
+
+
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     connectMongoDB();
